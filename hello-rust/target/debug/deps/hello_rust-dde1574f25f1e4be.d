07fcef213f2533512442f4fbecdc5b9b9ff8263a
lemonochrme <marin-muller@insa-toulouse.fr>
2025-03-03 23:36:43 +0100

    hello rust !

---
+++ hello-rust/target/debug/deps/hello_rust-dde1574f25f1e4be.d
@@ -0,0 +1,5 @@
+/home/robin/Documents/rust-exercices/hello-rust/target/debug/deps/hello_rust-dde1574f25f1e4be: src/main.rs
+
+/home/robin/Documents/rust-exercices/hello-rust/target/debug/deps/hello_rust-dde1574f25f1e4be.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/robin/Documents/rust-exercices/hello-rust/target/debug/hello-rust: /home/robin/Documents/rust-exercices/hello-rust/src/main.rs
